fc2e64568d1a61a3589daf20fb284814147d08c9
willianlemes <willianlemes94@gmail.com>
2020-10-15 19:15:33 -0300

    Inclusão das configurções de upload de imagens e conexão com o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import path from 'path';
+
+import './database/connection';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.send({ message: 'Hello World' });
-});
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);
